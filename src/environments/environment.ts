6f8fbd3cd109e653956d09f9da917e24b3f3fb12
espanderlof <jaime717@hotmail.es>
2024-12-12 23:19:04 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   apis: {
     usuarios: {
       // baseUrl: 'http://localhost:8081/api',
-      baseUrl: 'http://ip172-18-0-11-ctdo4pi91nsg00a5rei0-8081.direct.labs.play-with-docker.com/api',
+      baseUrl: 'http://ip172-18-0-29-ctdph50l2o9000bs0b60-8081.direct.labs.play-with-docker.com/api',
       endpoints: {
         base: 'usuarios',
         login: 'usuarios/login',
@@ -17,7 +17,7 @@ export const environment = {
     },
     productos: {
       // baseUrl: 'http://localhost:8082/api',
-      baseUrl: 'http://ip172-18-0-18-ctdo4pi91nsg00a5rei0-8082.direct.labs.play-with-docker.com/api',
+      baseUrl: 'http://ip172-18-0-47-ctdph50l2o9000bs0b60-8082.direct.labs.play-with-docker.com/api',
       endpoints: {
         base: 'productos',
         categoria: 'productos/categoria',
@@ -27,7 +27,7 @@ export const environment = {
     },
     ordenes: {
       // baseUrl: 'http://localhost:8083/api',
-      baseUrl: 'http://ip172-18-0-26-ctdo4pi91nsg00a5rei0-8083.direct.labs.play-with-docker.com/api',
+      baseUrl: 'http://ip172-18-0-20-ctdph50l2o9000bs0b60-8083.direct.labs.play-with-docker.com/api',
       endpoints: {
         base: 'orders',
         usuario: 'orders/user',
