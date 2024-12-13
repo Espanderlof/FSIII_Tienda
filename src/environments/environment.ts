322894bb12b9b27b1adddaf57d117b04e1796138
espanderlof <jaime717@hotmail.es>
2024-12-12 21:57:26 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: false,
   apis: {
     usuarios: {
-      baseUrl: 'http://localhost:8081/api',
-      // baseUrl: 'http://ip172-18-0-36-ctcev70l2o9000agp2v0-8081.direct.labs.play-with-docker.com/api',
+      // baseUrl: 'http://localhost:8081/api',
+      baseUrl: 'http://ip172-18-0-11-ctdo4pi91nsg00a5rei0-8081.direct.labs.play-with-docker.com/api',
       endpoints: {
         base: 'usuarios',
         login: 'usuarios/login',
@@ -16,8 +16,8 @@ export const environment = {
       }
     },
     productos: {
-      baseUrl: 'http://localhost:8082/api',
-      // baseUrl: 'http://ip172-18-0-31-ctcev70l2o9000agp2v0-8082.direct.labs.play-with-docker.com/api',
+      // baseUrl: 'http://localhost:8082/api',
+      baseUrl: 'http://ip172-18-0-18-ctdo4pi91nsg00a5rei0-8082.direct.labs.play-with-docker.com/api',
       endpoints: {
         base: 'productos',
         categoria: 'productos/categoria',
@@ -26,8 +26,8 @@ export const environment = {
       }
     },
     ordenes: {
-      baseUrl: 'http://localhost:8083/api',
-      // baseUrl: 'http://ip172-18-0-51-ctcev70l2o9000agp2v0-8083.direct.labs.play-with-docker.com/api',
+      // baseUrl: 'http://localhost:8083/api',
+      baseUrl: 'http://ip172-18-0-26-ctdo4pi91nsg00a5rei0-8083.direct.labs.play-with-docker.com/api',
       endpoints: {
         base: 'orders',
         usuario: 'orders/user',
